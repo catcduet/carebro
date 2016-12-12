00fbfa999f9c063aaea0077391765444d217b4c9
Nguyễn Minh Trang <pagenguyen219@gmail.com>
2016-12-12 15:05:55 +0700

    Convert all tabs to spaces

--- dataset.py
+++ dataset.py
@@ -12,73 +12,73 @@ HEIGHT = 18
 WIDTH = 273
 
 class Dataset():
-	def __init__(self, file_name):		
-		self.all_names = np.array(listdir(DATASET_FOLDER))  # read all file names in folder
-		self.all_labels = []  # [0, 272]
-		self.train_idx = []
-		self.val_idx = []
+    def __init__(self, file_name):      
+        self.all_names = np.array(listdir(DATASET_FOLDER))  # read all file names in folder
+        self.all_labels = []  # [0, 272]
+        self.train_idx = []
+        self.val_idx = []
 
-		# extract all labels from names
-		for i in range(self.all_names.shape[0]):
-			name = self.all_names[i].split(".")
-			self.all_labels.append(int(name[x]))
-		self.all_labels = np.array(self.all_labels, dtype='int')
+        # extract all labels from names
+        for i in range(self.all_names.shape[0]):
+            name = self.all_names[i].split(".")
+            self.all_labels.append(int(name[x]))
+        self.all_labels = np.array(self.all_labels, dtype='int')
 
-		if not file_name:
-			# create new training and validation set
-			self._create_new_train_val_set()
-		else:
-			# read dataset from files
-			self._read_train_val_set(file_name)
+        if not file_name:
+            # create new training and validation set
+            self._create_new_train_val_set()
+        else:
+            # read dataset from files
+            self._read_train_val_set(file_name)
 
-		self.train_idx = np.array(self.train_idx, dtype='int')
-		self.val_idx = np.array(self.val_idx, dtype='int')
-		
+        self.train_idx = np.array(self.train_idx, dtype='int')
+        self.val_idx = np.array(self.val_idx, dtype='int')
+        
 
-	def get_train_dataset(self):
-		return self._get_dataset(self.train_idx)
+    def get_train_dataset(self):
+        return self._get_dataset(self.train_idx)
 
-	def get_val_dataset(self):
-		return self._get_dataset(self.val_idx)
+    def get_val_dataset(self):
+        return self._get_dataset(self.val_idx)
 
-	def _get_dataset(self, idx_list):
-		m = idx_list.shape[0]
-		X = np.zeros((m, HEIGHT * WIDTH))
-		Y = np.zeros((m, WIDTH), dtype='int')
-		I = np.eye(WIDTH)
+    def _get_dataset(self, idx_list):
+        m = idx_list.shape[0]
+        X = np.zeros((m, HEIGHT * WIDTH))
+        Y = np.zeros((m, WIDTH), dtype='int')
+        I = np.eye(WIDTH)
 
-		for i in range(m):
-			img_idx = idx_list[i]
-			img_src = DATASET_FOLDER + self.all_names[img_idx] + ".png"
-			# load image as grayscale
-			img = cv2.imread(img_src, cv2.IMREAD_GRAYSCALE)
-			# flat image
-			img_flat = img.flatten() / 255.0  # normalize from [0, 255] to [0, 1]
-			X[i] = img_flat
-			Y[i, :] = I[self.all_labels[img_idx], :]
-			
-		X = X.reshape(m, 1, HEIGHT, WIDTH).astype('float32')
+        for i in range(m):
+            img_idx = idx_list[i]
+            img_src = DATASET_FOLDER + self.all_names[img_idx] + ".png"
+            # load image as grayscale
+            img = cv2.imread(img_src, cv2.IMREAD_GRAYSCALE)
+            # flat image
+            img_flat = img.flatten() / 255.0  # normalize from [0, 255] to [0, 1]
+            X[i] = img_flat
+            Y[i, :] = I[self.all_labels[img_idx], :]
+            
+        X = X.reshape(m, 1, HEIGHT, WIDTH).astype('float32')
 
-		return X, Y
+        return X, Y
 
-	def _create_new_train_val_set(self, ratio=0.8):
-		# select training set and validation set from data randomly
-		for i in range(self.num_data):
-			r = random.random()
-			if r < ratio:
-				self.train_idx.append(i)
-			else:
-				self.val_idx.append(i)
+    def _create_new_train_val_set(self, ratio=0.8):
+        # select training set and validation set from data randomly
+        for i in range(self.num_data):
+            r = random.random()
+            if r < ratio:
+                self.train_idx.append(i)
+            else:
+                self.val_idx.append(i)
 
-		# save their indexes to files
-		with open("train_val_set_{}".format(int(time.time())), "w") as f:
-			f.write(json.dumps({
-				"train_idx": self.train_idx,
-				"val_idx": self.val_idx
-			}))
+        # save their indexes to files
+        with open("train_val_set_{}".format(int(time.time())), "w") as f:
+            f.write(json.dumps({
+                "train_idx": self.train_idx,
+                "val_idx": self.val_idx
+            }))
 
-	def _read_train_val_set(self, file_name):
-		with open(file_name, "r") as f:
-			obj = f.read()
-		self.train_idx = obj["train_idx"]
-		self.val_idx = obj["val_idx"]
+    def _read_train_val_set(self, file_name):
+        with open(file_name, "r") as f:
+            obj = json.loads(f.read())
+        self.train_idx = obj["train_idx"]
+        self.val_idx = obj["val_idx"]
--- deeplane_model.py
+++ deeplane_model.py
@@ -22,57 +22,57 @@ X_val, Y_val = d.get_val_dataset()
 
 
 def build_model():
-	print("Building the model...")
-	model = Sequential()
-	model.add(Convolution2D(
-		nb_filter=32,
-		nb_row=18,
-		nb_col=18,
-		subsample=(6, 6),
-		border_mode='valid',
-		dim_ordering='th',
-		input_shape=(1, height, width),
-		activation='relu'
-	))
-	model.add(BatchNormalization())
-	model.add(MaxPooling2D(pool_size=(3, 3)))
+    print("Building the model...")
+    model = Sequential()
+    model.add(Convolution2D(
+        nb_filter=32,
+        nb_row=18,
+        nb_col=18,
+        subsample=(6, 6),
+        border_mode='valid',
+        dim_ordering='th',
+        input_shape=(1, height, width),
+        activation='relu'
+    ))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(3, 3)))
 
-	model.add(Convolution2D(
-		nb_filter=64,
-		nb_row=10,
-		nb_col=10,
-		subsample=(1, 1),
-		activation='relu'
-	))
-	model.add(BatchNormalization())
-	model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(Convolution2D(
+        nb_filter=64,
+        nb_row=10,
+        nb_col=10,
+        subsample=(1, 1),
+        activation='relu'
+    ))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(3, 3)))
 
-	model.add(Dense(2048, activation='relu'))
-	model.add(Dropout(0.5))
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.5))
 
-	model.add(Dense(2048, activation='relu'))
-	model.add(Dropout(0.5))
+    model.add(Dense(2048, activation='relu'))
+    model.add(Dropout(0.5))
 
-	model.add(Dense(WIDTH, activation='softmax'))
+    model.add(Dense(WIDTH, activation='softmax'))
 
-	model.compile(
-		loss='categorical_crossentropy',
-		optimizer='adam',
-		metrics=['accuracy']
-	)
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
 
-	return model
+    return model
 
 
 def train_model(model):
-	print("Training the model...")
-	# how many examples to look at during each training iteration
-	batch_size = 128
-	# how many times to run through the full set of examples
-	n_epochs = 10
-	# the training may be slow depending on your computer
-	model.fit(X_train,
-			Y_train,
-			batch_size=batch_size,
-			nb_epoch=n_epochs,
-			validation_data=(X_val, Y_val))
+    print("Training the model...")
+    # how many examples to look at during each training iteration
+    batch_size = 128
+    # how many times to run through the full set of examples
+    n_epochs = 10
+    # the training may be slow depending on your computer
+    model.fit(X_train,
+            Y_train,
+            batch_size=batch_size,
+            nb_epoch=n_epochs,
+            validation_data=(X_val, Y_val))
